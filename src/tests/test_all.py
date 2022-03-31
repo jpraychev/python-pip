c19c5925ff7492d7bf87e6321dc8a94ae6297bed
Jordan Raychev <jpraychev@gmail.com>
2022-03-31 06:15:06 +0300

    Added cleanup/teardown logic to tests

--- src/tests/test_all.py
+++ src/tests/test_all.py
@@ -1,30 +1,66 @@
 from python_pip import pip
+from pytest import fixture
 
 pip = pip.PIP()
 
+@fixture(scope='session', autouse=True)
+def cleanup(request):
+    print('\nSetting up...')
+    def fin():
+        print('\nCleaning up...')
+        pip.uninstall('requests')
+    request.addfinalizer(fin)
+
+@fixture
+def package_list():
+    return pip.list_packages()
+
+def install_package(package):
+    return pip.install(package)
+
+def uninstall_package(package):
+    return pip.uninstall(package)
+
 def test_version():
     assert pip.version == '0.33'
-
-def test_package_list():
+    
+def test_package_list(package_list):
     """ If pip is installed, underlying pip freeze command that is used from 
     list_packages() should return atleast one package """
-    assert len(pip.list_packages()) >= 1
+    assert len(package_list) >= 1
 
 def test_package_install_fail():
     """ Try to install unreleased requests version """
     output = pip.install('requests==0.001')
     assert 'could not be installed' in output
 
-def test_package_install_success():
+def test_package_install(package_list):
+    """ Test to install latest version of requests library """
     """ Try to install latest version of requests """
-    output = pip.install('requests')
-    assert 'Successfully installed requests' in output
+    if not 'requests' in package_list:
+        output = pip.install('requests')
+        assert 'Successfully installed requests' in output
+    else:
+        uninstall_package('requests')
+        output = pip.install('requests')
+        assert 'Successfully installed requests' in output
+    
+def test_package_uninstall(package_list):
+    """ Test to uninstall latest version of requests library """
+    if 'requests' in package_list:
+        output = uninstall_package('requests')
+        assert 'Successfully uninstalled requests' in output
+    else:
+        install_package('requests')
+        output = uninstall_package('requests')
+        assert 'Successfully uninstalled requests' in output
 
-def test_package_already_installed():
-    """ Try to install latest version of requests """
-    output = pip.install('requests')
-    assert 'Requirement already satisfied' in output
-
-def test_package_uninstall_success():
-    output = pip.uninstall('requests')
-    assert 'Successfully uninstalled requests' in output
+def test_package_already_installed(package_list):
+    """ Test if latest version of requests library is already installed """
+    if 'requests' in package_list:
+        output = install_package('requests')
+        assert 'Requirement already satisfied' in output
+    else:
+        install_package('requests')
+        output = install_package('requests')
+        assert 'Requirement already satisfied' in output
